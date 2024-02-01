c7c492ec74b75eca9059f87a4749d69e5f1579f6
berkin09 <berkin09@yandex.kz>
2024-02-01 21:48:43 +0600

    Добавление КР

--- learning/JS/lesson-6 For/script.js
+++ learning/JS/lesson-6 For/script.js
@@ -1,5 +1,18 @@
-let sum=0;
-for (let i=1; i<=10; i++) {
-    sum=sum+i;
+// let sum=0;
+// for (let i=1; i<=10; i++) {
+//     sum=sum+i;
+// }
+// console.log(sum)
+
+let num=100;
+let even;
+let fact=1;
+
+for (let i=1; i<=num; i++) {
+    if(i%2===0) {
+        console.log('Четное число'+i)
+        even=i**2
+        console.log('Четное число в квадрате ='+even)
+    }
 }
-console.log(sum)
+    
