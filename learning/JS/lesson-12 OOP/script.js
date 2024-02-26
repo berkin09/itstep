9c08f1be8b0d0d343828b2e40c5f703c6d7dd9fe
Berkin <example@gmail.com>
2024-02-26 17:51:33 +0600

    sss

--- learning/JS/lesson-12 OOP/script.js
+++ learning/JS/lesson-12 OOP/script.js
@@ -130,7 +130,6 @@
 // timeSet.addSeconds(120);
 // console.log("После добавление 120 секунд:", timeSet.displayTime());
 
-
 // 4. Создайте класс для телефона (имя бренд цена и фотография, метод отображения карточек в html, также использовать css). Отобразите три карточки с телефонами.
 // Используя массив для отображения, в котором объекты трех телефонов, созданных через класс.
 // class Telephone {
@@ -160,3 +159,47 @@
 // for (let i = 0; i < phones.length; i++) {
 //     phones[i].displayHTML();
 // }
+
+/*Создайте класс для описания машины. С параметрами. Вес, цвет, скорость марка и т.д. Чем больше, чем лучше. 
+Создайте также различные методы. Например, метод для получения информации сколько километров пройдёт машина с такой скоростью за переданное её время и т.д. 
+Создайте несколько объектов разных машин и измените им некоторые параметры и выведите результаты. */
+
+class Automobile {
+  constructor(model, color, weight, speed, year, img) {
+    this.model = model;
+    this.color = color;
+    this.weight = weight;
+    this.speed = speed;
+    this.year = year;
+    this.img = img;
+  }
+
+  displayHTML() {
+    const autoCardHTML = `
+        <div class="auto-card">
+            <strong>Марка:</strong> ${this.model}<br>
+            <strong>Цвет:</strong> ${this.color}<br>
+            <strong>Вес:</strong> ${this.weight} кг.<br>
+            <strong>Скорость:</strong> ${this.speed}<br>
+            <strong>Год:</strong> ${this.year}<br>
+            <img class="phone-img" src="${this.img}" alt="${this.model}">
+        </div>
+    `;
+    document.write(autoCardHTML);
+  }
+}
+
+const autos = [
+  new Automobile(
+    "Hyundai Sonata Hybrid",
+    "Серый",
+    1586,
+    159,
+    2023,
+    "https://www.hyundai.ru/storage/news/42019-7-22_News_41image.jpg"
+  ),
+];
+
+for (let i = 0; i < autos.length; i++) {
+  autos[i].displayHTML();
+}
