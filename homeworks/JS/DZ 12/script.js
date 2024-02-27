5ee7cbb71c70fee37fb20e43e39ea9d1b0882f9e
Berkin <example@gmail.com>
2024-02-27 18:15:29 +0600

    ДЗ

--- homeworks/JS/DZ 12/script.js
+++ homeworks/JS/DZ 12/script.js
@@ -3,41 +3,39 @@
 Создайте несколько объектов разных машин и измените им некоторые параметры и выведите результаты. */
 
 class Automobile {
-  constructor(model, color, weight, speed, year, img) {
+  constructor(model, color, weight, speed, year, engineCapacity) {
     this.model = model;
     this.color = color;
     this.weight = weight;
     this.speed = speed;
     this.year = year;
-    this.img = img;
+    this.engineCapacity = engineCapacity;
   }
-
-  displayHTML() {
-    const autoCardHTML = `
-          <div class="auto-card">
-              <strong>Марка:</strong> ${this.model}<br>
-              <strong>Цвет:</strong> ${this.color}<br>
-              <strong>Вес:</strong> ${this.weight} кг.<br>
-              <strong>Скорость:</strong> ${this.speed}<br>
-              <strong>Год:</strong> ${this.year}<br>
-              <img class="car-img" src="${this.img}" alt="${this.model}">
-          </div>
-      `;
-    document.write(autoCardHTML);
+  getInfo() {
+    return `Модель машины: ${this.model} 
+    Цвет: ${this.color}
+    Вес: ${this.weight}
+    Максимальная скорость: ${this.speed}
+    Год выпуска: ${this.year}
+    Объем двигателя: ${this.engineCapacity}`;
+  }
+  getSpeed(time) {
+    return `При максимальной скорости ${this.model} за ${time} минут проедет ${
+      this.speed * (time / 60)
+    } км`;
   }
 }
 
 const autos = [
-  new Automobile(
-    "Hyundai Sonata Hybrid",
-    "Серый",
-    1586,
-    159,
-    2023,
-    "https://www.hyundai.ru/storage/news/42019-7-22_News_41image.jpg"
-  ),
+  new Automobile("Hyundai Sonata", "Серый", 1980, 210, 2023, 2.5),
+  new Automobile("Lada Priora", "Черный", 1163, 183, 2023, 1.6),
+  new Automobile("Toyota Corolla", "Белый", 1370, 195, 2023, 1.6),
 ];
 
-for (let i = 0; i < autos.length; i++) {
-  autos[i].displayHTML();
+for (let i = 0; i <= autos.length - 1; i++) {
+  console.log(autos[i].getInfo());
+}
+
+for (let i = 0; i <= autos.length - 1; i++) {
+  console.log(autos[i].getSpeed(60));
 }
