bcd0d9d2ccb99323d3b369ac032216e339415eec
berkin09 <berkin09@yandex.kz>
2024-02-27 21:51:01 +0600

    КР

---
+++ learning/JS/lesson-14 try catch refactoring/script.js
@@ -0,0 +1,67 @@
+// let num = 10;
+
+// try {
+//     num = num + 5;
+//     console.log(num)
+// } catch(error) {
+//     console.log('Тут ошибка')
+// }
+
+// console.log('Next Code')
+// console.log('Next Code')
+// console.log('Next Code')
+// console.log('Next Code')
+
+// const num = 10;
+
+// try {
+//     num = num + 5;
+//     console.log(num)    
+// } catch(e) {
+//     console.error('Тут ошибка')
+// }
+
+// try {
+//     let pow = num ** 2;
+//     console.log(pow)
+// } catch(e) {
+//     console.error('Тут ошибка 2')
+// }
+
+
+// 1 задача
+// const x = function(z) {
+//     let w = 0;
+//     z.forEach (function(q){
+//             w += q;
+//         });
+//         return w;
+//     };
+
+// x([2, 2, 2]);
+
+
+// 2 задача
+
+// const selected = isSelected = true;
+// const numberOfThings = getCount = 10;
+// const myName = "Тор";
+
+// 3 задача
+// function getCount () {
+//     return numberOfThings;
+//   }
+//   function myName () {
+//     return "Тор";
+//   }
+  
+
+
+// let studentName = "Лера"
+// let profession = "React-разработчик"
+// function showStudentIntention () {
+//   alert(studentName + " хочет освоить профессию " + profession);
+// }
+
+// showStudentIntention();
+
