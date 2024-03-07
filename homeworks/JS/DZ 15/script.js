b9a17dde9f47af859ac4e3961fa01a6be49d72a2
Berkin <example@gmail.com>
2024-03-07 10:08:43 +0500

    asdasd

--- homeworks/JS/DZ 15/script.js
+++ homeworks/JS/DZ 15/script.js
@@ -1,46 +1,59 @@
-let car = {
-  brand: "Toyota",
-  model: "Camry",
-  productionDate: new Date(2022, 0, 1),
-  averageSpeed: 100,
-};
+/*1. Создать объект, описывающий автомобиль (производитель, модель, год выпуска, средняя скорость), и следующие функции для работы с этим объектом.
+• Функция для вывода на экран информации об автомобиле.
+• Функция для подсчета необходимого времени для преодоления переданного расстояния.*/
 
-var options = {
-  // era: 'long',
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-  weekday: "long",
-  // timezone: 'UTC',
-  // hour: 'numeric',
-  // minute: 'numeric',
-  // second: 'numeric'
-};
+// let car = {
+//   brand: "Toyota",
+//   model: "Camry",
+//   productionDate: new Date(2022, 0, 1),
+//   averageSpeed: 100,
+// };
 
-function displayCarInfo(car) {
-  console.log(`Марка: ${car.brand}`);
-  console.log(`Модель: ${car.model}`);
-  console.log(`Дата выпуска: ${car.productionDate.toDateString()}`);
-  console.log(`Средняя скорость: ${car.averageSpeed} км`);
-}
+// var options = {
+//   year: "numeric",
+//   month: "long",
+//   day: "numeric",
+//   weekday: "long",
+// };
 
-function calculateTime(distance, car) {
-  const currentDate = new Date();
-  const timeInHours = distance / car.averageSpeed;
-  const estimatedArrival = new Date(
-    car.productionDate.getTime() + timeInHours * 60 * 60 * 1000
-  );
+// function displayCarInfo(car) {
+//   console.log(`Марка: ${car.brand}`);
+//   console.log(`Модель: ${car.model}`);
+//   console.log(
+//     `Дата выпуска: ${car.productionDate.toLocaleString("ru-RU", options)}`
+//   );
+//   console.log(`Средняя скорость: ${car.averageSpeed} км`);
+// }
 
-  console.log(`Текущая дата: ${currentDate.toDateString("ru", options)}`);
-  console.log(
-    `Ожидаемое прибытие: ${estimatedArrival.toDateString()} ${estimatedArrival.toLocaleTimeString()}`
-  );
-}
+// function calculateTime(distance, car) {
+//   const currentDate = new Date();
+//   const timeInHours = distance / car.averageSpeed;
+//   const estimatedArrival = new Date(
+//     currentDate.getTime() + timeInHours * 60 * 60 * 1000
+//   );
 
-displayCarInfo(car);
-calculateTime(120, car);
+//   console.log(
+//     `Текущая дата и время: ${currentDate.toLocaleString(
+//       "ru-RU",
+//       options
+//     )} ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`
+//   );
 
-/*// Создание объекта "Дробь"
+//   console.log(
+//     `Ожидаемое прибытие: ${estimatedArrival.toLocaleString()} ${estimatedArrival.toLocaleTimeString()}`
+//   );
+// }
+
+// displayCarInfo(car);
+// calculateTime(120, car);
+
+/*2. Создать объект, хранящий в себе отдельно числитель и знаменатель дроби, и следующие функции для работы с этим объектом.
+• Функция сложения 2-х объектов-дробей.
+• Функция вычитания 2-х объектов-дробей.
+• Функция умножения 2-х объектов-дробей.
+• Функция деления 2-х объектов-дробей.
+• Функция сокращения объекта-дроби.*/
+
 let fraction1 = {
   numerator: 1,
   denominator: 2,
@@ -51,10 +64,12 @@ let fraction2 = {
   denominator: 4,
 };
 
-// Функция сложения двух дробей
+// Функция сложения 2-х объектов-дробей.
 function addFractions(fraction1, fraction2) {
   return {
-    numerator: fraction1.numerator * fraction2.denominator + fraction2.numerator * fraction1.denominator,
+    numerator:
+      fraction1.numerator * fraction2.denominator +
+      fraction2.numerator * fraction1.denominator,
     denominator: fraction1.denominator * fraction2.denominator,
   };
 }
@@ -62,7 +77,9 @@ function addFractions(fraction1, fraction2) {
 // Функция вычитания двух дробей
 function subtractFractions(fraction1, fraction2) {
   return {
-    numerator: fraction1.numerator * fraction2.denominator - fraction2.numerator * fraction1.denominator,
+    numerator:
+      fraction1.numerator * fraction2.denominator -
+      fraction2.numerator * fraction1.denominator,
     denominator: fraction1.denominator * fraction2.denominator,
   };
 }
@@ -98,4 +115,4 @@ function simplifyFraction(fraction) {
 console.log("Sum:", addFractions(fraction1, fraction2));
 console.log("Difference:", subtractFractions(fraction1, fraction2));
 console.log("Product:", multiplyFractions(fraction1, fraction2));
-console.log("Quotient:", divideFractions(fraction1, fraction2)); */
+console.log("Quotient:", divideFractions(fraction1, fraction2));
